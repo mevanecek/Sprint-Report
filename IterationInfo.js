129384a4730cdaf394aa48edffece11a3ef5653f
mevanecek <matt@mattvanecek.com>
2017-03-10 16:35:03 -0600

    Refactoring

--- IterationInfo.js
+++ IterationInfo.js
@@ -5,45 +5,44 @@
         extend: 'Ext.panel.Panel',
         alias: 'widget.iterationinforow',
         cls: 'iterationInfoRow',
-        requires: [ 'Rally.data.wsapi.Store' ],
+        requires: ['Rally.data.wsapi.Store'],
 
-        items: [
-        {
-            xtype: 'panel',
-            border: 0,
-            flex: 1,
-            itemId: 'leftPanel'
-        },
-        {
-            xtype: 'panel',
-            border: 0,
-            flex: 1,
-            itemId: 'rightPanel'
-        }
-            ],
+        items: [{
+                xtype: 'panel',
+                border: 0,
+                flex: 1,
+                itemId: 'leftPanel'
+            },
+            {
+                xtype: 'panel',
+                border: 0,
+                flex: 1,
+                itemId: 'rightPanel'
+            }
+        ],
         layout: {
             type: 'hbox',
             align: 'stretch'
         },
 
-//        renderTo: Ext.getBody(),
+        //        renderTo: Ext.getBody(),
 
         leftTemplate: new Ext.Template('<table style="margin-left: 25px;"><tr><td style="font-weight: bold;">Sprint:</td><td>{sprintName}</td></tr>' +
-                                       '<tr><td style="font-weight: bold;">Start Date:</td><td>{startDate}</td></tr>' +
-                                       '<tr><td style="font-weight: bold;">End Date:</td><td>{endDate}</td></tr></table>'),
-            rightTemplate:  new Ext.Template('<table style="margin-left: 25px;"><tr><td style="font-weight: bold;">Theme:</td></tr><tr><td>{sprintTheme}</td></tr></table>'),
+            '<tr><td style="font-weight: bold;">Start Date:</td><td>{startDate}</td></tr>' +
+            '<tr><td style="font-weight: bold;">End Date:</td><td>{endDate}</td></tr></table>'),
+        rightTemplate: new Ext.Template('<table style="margin-left: 25px;"><tr><td style="font-weight: bold;">Theme:</td></tr><tr><td>{sprintTheme}</td></tr></table>'),
 
-            store: null,
+        store: null,
         iteration: null,
 
-//        mixins: {
-//            observable: 'Ext.util.Observable'
-//        },
+        //        mixins: {
+        //            observable: 'Ext.util.Observable'
+        //        },
 
         constructor: function(config) {
             this.mergeConfig(config);
 
-//            this.mixins.observable.constructor.call(this, config);
+            //            this.mixins.observable.constructor.call(this, config);
             this.addEvents('loaded');
             this.callParent(arguments);
         },
@@ -51,12 +50,12 @@
         _load: function(store, data) {
             var startDt = Rally.util.DateTime.format(data[0].data.StartDate, 'M d, Y');
             var endDt = Rally.util.DateTime.format(data[0].data.EndDate, 'M d, Y');
-            var leftHtml = this.leftTemplate.apply( {
+            var leftHtml = this.leftTemplate.apply({
                 sprintName: data[0].data.Name,
                 startDate: startDt,
                 endDate: endDt
             });
-            var rightHtml = this.rightTemplate.apply( { sprintTheme: data[0].data.Theme });
+            var rightHtml = this.rightTemplate.apply({ sprintTheme: data[0].data.Theme });
 
             //            console.log("Iteration = %o\n", data);
             this.getComponent('leftPanel').update(leftHtml);
@@ -76,10 +75,10 @@
                     },
                     fetch: [
                         'Name', 'StartDate', 'EndDate', 'Theme'
-                        ]
+                    ]
                 });
             }
-            this.store.setFilter( {
+            this.store.setFilter({
                 property: 'ObjectID',
                 value: iter
             });
@@ -92,4 +91,3 @@
 
     });
 })();
-
--- SprintReportCharts.js
+++ SprintReportCharts.js
@@ -1,4 +1,3 @@
-
 (function() {
     var Ext = window.Ext4 || window.Ext;
 
@@ -6,33 +5,32 @@
         extend: 'Ext.panel.Panel',
         alias: 'widget.sprintreportcharts',
         cls: 'sprint-report-charts',
-        requires: [ 'Rally.data.wsapi.Store' ],
+        requires: ['Rally.data.wsapi.Store'],
 
-        items: [
-        {
-            xtype: 'panel',
-            border: 1,
-            flex: 1,
-            title: 'Sprint Burndown',
-            layout: 'fit',
-            id: 'leftChartsPanelID',
-            itemId: 'leftChartsPanel'
-        },
-        {
-            xtype: 'panel',
-            border: 1,
-            flex: 1,
-            layout: 'fit',
-            id: 'rightChartsPanelID',
-            itemId: 'rightChartsPanel'
-        }
-            ],
+        items: [{
+                xtype: 'panel',
+                border: 1,
+                flex: 1,
+                title: 'Sprint Burndown',
+                layout: 'fit',
+                id: 'leftChartsPanelID',
+                itemId: 'leftChartsPanel'
+            },
+            {
+                xtype: 'panel',
+                border: 1,
+                flex: 1,
+                layout: 'fit',
+                id: 'rightChartsPanelID',
+                itemId: 'rightChartsPanel'
+            }
+        ],
         layout: {
             type: 'hbox',
             align: 'stretch'
         },
 
-            store: null,
+        store: null,
         iteration: null,
 
         constructor: function(config) {
@@ -42,24 +40,23 @@
         },
 
         _load: function() {
-            var iters = [ this.iteration ];
+            var iters = [this.iteration];
             var iterBurn = Ext.create('Rally.ui.report.StandardReport', {
                 reportConfig: {
-                        report: 'IterationBurndown',
-                        iterations: iters,
-                        legend: 'show'
-                    },
+                    report: 'IterationBurndown',
+                    iterations: iters,
+                    legend: 'show'
+                },
                 width: 400,
                 height: 400
-                });
+            });
             console.log("item 0 : %o\n", this.items.getAt(0));
             this.items.getAt(0).add(iterBurn);
         },
-        
+
         _setIterationId: function(iter) {
             this.iteration = iter;
         }
 
     });
 })();
-
